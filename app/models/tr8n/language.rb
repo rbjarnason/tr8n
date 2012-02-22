f1cd1d851a7d44931c296aa21cb97cf63b8778d2
Róbert Viðar Bjarnason <robert.bjarnason@gmail.com>
2012-02-22 19:15:45 +0000

    Added default locale lookup from iso country list, mapping information about most used language in each country of the world

--- app/models/tr8n/language.rb
+++ app/models/tr8n/language.rb
@@ -35,6 +35,10 @@ class Tr8n::Language < ActiveRecord::Base
   has_many :translations,           :class_name => 'Tr8n::Translation',         :dependent => :destroy
   has_many :translation_key_locks,  :class_name => 'Tr8n::TranslationKeyLock',  :dependent => :destroy
   has_many :language_metrics,       :class_name => 'Tr8n::LanguageMetric'
+
+  has_and_belongs_to_many :iso_countries, :class_name => 'Tr8n::IsoCountry', :foreign_key => "tr8n_language_id", :association_foreign_key=>"tr8n_iso_country_id",  :join_table=>"tr8n_iso_countries_tr8n_languages"
+
+  belongs_to :fallback_language,    :class_name => 'Tr8n::Language', :foreign_key => :fallback_language_id
   
   def self.find_or_create(lcl, english_name)
     find_by_locale(lcl) || create(:locale => lcl, :english_name => english_name) 
